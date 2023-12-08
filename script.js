131b7f341f73cc193fb5cc85b563f92bddf46732
Neaht <thaenthai124@gmail.com>
2023-12-08 16:42:51 +0700

    [FIX] remove no button after click yes button

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you tmr bibi";
+  question.innerHTML = "Yay, see you tmr bibi!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+    noBtn.style.display = "none";
 });
 
 noBtn.addEventListener("mouseover", () => {
